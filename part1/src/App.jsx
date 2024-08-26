d1c034c2f5227e0a30e4516d35ddc70b67f62254
Abdullah Hared <muxsinxareed6@gmail.com>
2024-08-26 08:04:55 +0200

    Finish the 1st exercise

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,29 +1,36 @@
-const Hello = (props) => {
-
-  console.log(props)
-  return (
-    <div>
-      <p>
-
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
-
 const App = () => {
-
-  const name = 'Peter'
-  const age = 10
-
   return (
     <div>
-      <h1>Greetings</h1>
-
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Header />
+      <Content />
+      <Footer />
     </div>
   )
 }
 
-export default App;
+const Header = () => {
+  const course = 'Half Stack application development';
+  return <h1>{course}</h1>;
+} 
+
+const Content = () => {
+  const part1 = 'Fundamentals of React';
+  const part2 = 'Using props to pass data';
+  const part3 = 'State of a component';
+  return (
+    <div>
+      part1: {part1} <br />
+      part2: {part2} <br />
+      part3: {part3} <br />
+    </div>
+  );
+}
+
+const Footer = () => {
+  const exercise1 = 10;
+  const exercise2 = 7;
+  const exercise3 = 14;
+  return <p>Number of exercises {exercise1 + exercise2 + exercise3}</p>;
+}
+
+export default App
