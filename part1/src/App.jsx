2e568693663879f2fe76ab6aef216031895bd4bd
Abdullah Hared <muxsinxareed6@gmail.com>
2024-08-26 12:39:13 +0200

    Learned how to use component states and eventHandlers

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,39 +1,48 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 }
-  ];
+  const [counter, setCounter] = useState(0)
+
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Footer parts={parts} />
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
-  );
-};
-
-const Header = ({ course }) => {
-  return <h1>{course}</h1>;
-};
-
-const Content = ({ parts }) => {
+  )
+} 
+const Button = (props) => {
   return (
-    <div>
-      {parts.map((part) => (
-        <p key={part.name}>
-          {part.name} {part.exercises}
-        </p>
-      ))}
-    </div>
-  );
-};
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
 
-const Footer = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p>Number of exercises {totalExercises}</p>;
-};
-
-export default App;
+export default App
